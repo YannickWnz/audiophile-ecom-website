581322f90ade44421bf035ae7637ab57d34f659d
yannickwnz <yannickwnz@gmail.com>
2023-05-10 12:19:20 +0000

    first commit

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import './Navbar.scss'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar-container">
+                <h1>hey yooo</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
