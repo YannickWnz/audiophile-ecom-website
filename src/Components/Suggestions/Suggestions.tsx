fe65cf588dcb063efab76503adb3145fa859ce1c
yannickwnz <yannickwnz@gmail.com>
2023-07-14 09:29:21 +0000

    products suggestions component features

--- src/Components/Suggestions/Suggestions.tsx
+++ src/Components/Suggestions/Suggestions.tsx
@@ -2,32 +2,60 @@ import { Button } from '../Button/Button'
 import './Suggestion.scss'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import {headphonesProductDetails} from '../../Components/Interfaces'
 
 
-export const Suggestion = () => {
+interface ProductSuggestionID {
+    id?: string
+}
 
-    let img: string = 'assets/product-xx99-mark-one-headphones/desktop/image-product.jpg'
-    let img1: string = 'assets/product-xx99-mark-two-headphones/desktop/image-product.jpg'
-    let img2: string = 'assets/product-xx59-headphones/desktop/image-product.jpg'
 
-    
+export const Suggestion = ({id}: ProductSuggestionID) => {
+
+    const [suggestedProduct, setSuggestedProduct] = useState<headphonesProductDetails[]>([])
+
+    let img: string = '/assets/product-xx99-mark-one-headphones/desktop/image-product.jpg'
+    let img1: string = '/assets/product-xx99-mark-two-headphones/desktop/image-product.jpg'
+    let img2: string = '/assets/product-xx59-headphones/desktop/image-product.jpg'
+
+
+    const fetchYouMayAlsoLikeProducts = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8800/productSuggestion/${id}`)
+            setSuggestedProduct(response.data)
+            // console.log(suggestedProduct, 'hey')
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        fetchYouMayAlsoLikeProducts()
+    }, [id])
+
+    function getProductImage(index: number): string {
+        const path: string[] = JSON.parse(suggestedProduct[index].imagePath)
+        return path[0]
+    }
 
 
     return (
         <div className="suggestions">
             <h1>YOU MAY ALSO LIKE</h1>
             <div className="product-suggestions">
-                <div className="product-item">
+                {suggestedProduct.length > 0 && suggestedProduct.map((product, index) => {
+                    return  <div key={product.id} className="product-item">
                     <div 
                     className="item-img-bg"
-                    style={{backgroundImage: `url(${img})`}}
+                    style={{backgroundImage: `url(/${getProductImage(index)})`}}
                     ></div>
                     <div className="item-desc">
-                        <h2>XX99 MARK I</h2>
-                        <Button />
+                        {/* <h2>XX99 MARK I</h2> */}
+                        <h2>{product.name.toUpperCase()}</h2>
+                        <Button id={product.id} />
                     </div>
-                </div>
-                <div className="product-item">
+                </div>})}
+                {/* <div className="product-item">
                     <div 
                     className="item-img-bg"
                     style={{backgroundImage: `url(${img1})`}}
@@ -46,7 +74,7 @@ export const Suggestion = () => {
                         <h2>XX59</h2>
                         <Button />
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
